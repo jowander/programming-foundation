85ea8ed57a16fe0cf145c9ab5ef0b31ab0f6eedb
John Willy Andersen <74373353+jowander@users.noreply.github.com>
2022-01-09 21:52:52 +0100

    Add files via upload

---
+++ programming-foundation/js/script.js
@@ -0,0 +1,46 @@
+//Question 1
+
+var myCars = 15;
+console.log(myCars);
+
+var myStreet = "Trafikkgata";
+console.log(myStreet);
+
+var movedToKristiansand = true;
+console.log(movedToKristiansand);
+
+//Question 2
+
+var firstName = "John";
+var middleName = "Willy";
+var lastName = "Andersen";
+
+var fullName = firstName + " " + middleName + " " + lastName;
+
+console.log(fullName);
+
+//Question 3
+
+var animal = "frog";
+
+console.log("The type of frog is" + " " + typeof animal);
+
+//Question 4
+
+var orderHasShipped = true;
+var notShipped = "The order has not shipped";
+var shipped = "The order has shipped";
+
+if (orderHasShipped === false) {
+    console.log(shipped)
+} else if (orderHasShipped === true) {
+    console.log(notShipped)
+};
+
+//Question 5
+
+for (var i = 7; i < 13; i++) {
+    console.log(i)
+};
+
+
